f3fd6b96ca9a72eab0773815b099d04198be36d1
ptzburn <milan.hokkanen@gmail.com>
2025-05-13 15:15:42 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/': { target: process.env.REACT_APP_API_URL, changeOrigin: true }
+    }
+  }
 })
